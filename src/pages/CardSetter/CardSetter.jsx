d72911f3b123ea200f7d6b49572522e49be7f849
Anuj Kumar <kumar_191851@student.nitw.ac.in>
2021-09-20 18:55:27 +0530

    Card Import Page Button design done...

--- src/pages/CardSetter/CardSetter.jsx
+++ src/pages/CardSetter/CardSetter.jsx
@@ -5,9 +5,18 @@ export default function CardSetter() {
   const solidTextLine = "Generate a card with Solid Background...";
   const imageInputTextLine = "Generate a card with Image as Background...";
   const [cardDescText, setCardDescText] = useState("");
+  const [inputDiv, setInputDiv] = useState(false);
+  const clickHandler = (e) => {
+    const name = e.target.className;
+    setInputDiv(true);
+    return name === "solidColor" ? <SolidModel /> : <ImageModel />;
+  };
 
   return (
-    <div className="cardSetter">
+    <div
+      className="cardSetter"
+      style={inputDiv ? { opacity: 0.5, pointerEvents: "none" } : {}}
+    >
       <div className="titleDiv">
         <span className="nameCard">Generate Card</span>
       </div>
@@ -15,8 +24,10 @@ export default function CardSetter() {
         <div className="solidDiv">
           <button
             className="solidColor"
+            title="solidColorButton"
             onMouseEnter={() => setCardDescText(solidTextLine)}
             onMouseLeave={() => setCardDescText("")}
+            onClick={clickHandler}
           >
             Solid
           </button>
@@ -26,14 +37,26 @@ export default function CardSetter() {
             className="imageInput"
             onMouseEnter={() => setCardDescText(imageInputTextLine)}
             onMouseLeave={() => setCardDescText("")}
+            onClick={clickHandler}
           >
             Image
           </button>
         </div>
       </div>
       <div className="cardDesc">
-        <span className="cardDescText">{cardDescText}</span>
+        <span className="cardDescText">
+          {/* {cardDescText} */}
+          Generate a card with Image as Background...
+        </span>
       </div>
     </div>
   );
+}
+
+function SolidModel() {
+  return <div className="solidModel">solid Model</div>;
+}
+
+function ImageModel() {
+  return <div className="imageModel">Image Model</div>;
 }
